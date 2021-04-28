f6fea4c2a5eff7819426bc3509b84c1094fbcb4e
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-04-28 19:48:21 +0200

    add structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import CardSection from "./CardSection";
+import Chart from "./Chart";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <CardSection />
+      <Chart />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Covid-19 Malta</h1>
+    </header>
+  );
+};
+
+export default Header;
